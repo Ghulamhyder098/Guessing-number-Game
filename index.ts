414a3b1295fe0cee0eb3b86a65f3b4972b8fabad
GHULAM HYDER <160345250+Ghulamhyder098@users.noreply.github.com>
2024-03-26 14:50:15 +0500

    Project-2 Guessing-Game
    Typescript Project Guessing A number Game

---
+++ index.ts
@@ -0,0 +1,32 @@
+// let guessNum = "14";
+// let answer = prompt("Guess The number");
+// // while (guessNum !== answer) {
+// //     answer = prompt("Try Again");
+// // }
+// let i=0
+// for (i=0;guessNum!==answer;i++){
+//     answer=prompt("Try Again")
+// }
+// console.log("Congratulations You Enter Right Number");
+
+import { log } from "console";
+import inquirer from "inquirer"
+let random=Math.floor(Math.random()*10+1);
+// console.log(random);
+let answer=await inquirer.prompt([{message:"Guess The number B/w 1-10",type:"number",name:"number"}])
+// console.log(answer)
+if (random===answer.number){
+    console.log("Congratulation")
+}else if(answer.number>10) {
+    console.log("Please Input Valid Number")
+}else{
+    console.log("Try Again")
+}
+
+
+
+
+
+
+
+
